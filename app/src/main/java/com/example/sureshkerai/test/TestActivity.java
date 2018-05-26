14acdd2ddcc7634c94ea8e2f6167210602b21b19
Bindiya <bindiyarathod2@gmail.com>
2018-05-26 10:29:55 +0530

    add comment TestActivity

--- app/src/main/java/com/example/sureshkerai/test/TestActivity.java
+++ app/src/main/java/com/example/sureshkerai/test/TestActivity.java
@@ -1,8 +1,10 @@
 package com.example.sureshkerai.test;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by apple on 26/05/18.
  */
 
-public class TestActivity {
+public class TestActivity extends AppCompatActivity{
 }
